5307c55ac78d8bf1ead868e4caa30fd45166c5b0
nisalrenuja <nrenuja@gmail.com>
2022-06-13 23:42:30 +0530

    icons coponent added

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href='www.facebook.com' target="_blank"></a>
+        <a href='www.twitter.com' target="_blank"></a>
+        <a href='www.linkedin.com' target="_blank"></a>
+        <a href='www.github.com' target="_blank"></a>
+        <a href='www.instagram.com' target="_blank"></a>
+        </div>
+    
+  )
+}
+
+export default HeaderSocials
