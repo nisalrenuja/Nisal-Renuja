6b495e6d689b96dd68c3f74fad31121e574c4553
nisalrenuja <nrenuja@gmail.com>
2022-06-22 11:40:11 +0530

    header modified

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import CTA from './CTA'
 import "./header.css"
-import ME from '../../assets/nisal.jpg'
+// import ME from '../../assets/nisal.jpg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className="container header __container">
+      <div className="header__container">
         <h5>Hello I'm</h5>
         <h1>Nisal Renuja Palliyaguru</h1>
         <h5 className='text-light'>FullStack Developer</h5>
         <CTA/>
         <HeaderSocials/>
-        <div className="me">
-           <img src={ME} alt='nisal renuja'></img> 
-        </div>
+        {/* <div className="me">
+          <img src={ME} alt='nisal renuja'></img>  
+        </div> */}
         <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
