67acb2359167c49abf96fb297bccf8d0ca5c6f7f
nisalrenuja <nrenuja@gmail.com>
2022-06-25 19:36:56 +0530

    minor fixes in styling

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/Nisal Palliyaguru Resume.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        {/* <a href={CV} download className='btn'>Download CV</a>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a> */}
     </div>
   )
 }
