595f36d6e69b8ab58e6928e298c0598793b64737
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-09-04 08:23:22 +0530

    links fixed

--- src/components/WorkList/index.js
+++ src/components/WorkList/index.js
@@ -1,39 +1,38 @@
-import React from "react"
-import * as Styled from "./styled"
-import { StaticImage } from "gatsby-plugin-image"
-import Pill from "../Pill"
+import React from "react";
+import * as Styled from "./styled";
+import { StaticImage } from "gatsby-plugin-image";
+import Pill from "../Pill";
 
 const WorkList = () => {
-    return (
-        <>
-            <Styled.SectionTitle>Projects</Styled.SectionTitle>
-            <Styled.StyledLink
-                to="/work/vice"
-                className="vice"
-            >
-                <Styled.ImageWrapper>
-                    <StaticImage
-                        src="../../images/work_list/vice.png"
-                        alt="VICE"
-                        placeholder="none"
-                        layout="constrained"
-                        quality="90"
-                    />
-                </Styled.ImageWrapper>
-                <div>
-                    <Styled.Title>My Dictionary</Styled.Title>
-                    <Styled.Dek>web application to search English words and their meanings.</Styled.Dek>
-                    <Pill>React</Pill>
-                    <Pill>Tailwind CSS</Pill>
-                </div>
-            </Styled.StyledLink>
-            <Styled.Masonry>
-                <Styled.MasonryColumn>
-                    <Styled.StyledLink
-                        to="/work/cover"
-                        className="cover"
-                    >
-                        {/* <Styled.ImageWrapper>
+  return (
+    <>
+      <Styled.SectionTitle>Projects</Styled.SectionTitle>
+      <Styled.StyledLink to="https://github.com/nisalrenuja" className="vice">
+        <Styled.ImageWrapper>
+          <StaticImage
+            src="../../images/work_list/vice.png"
+            alt="VICE"
+            placeholder="none"
+            layout="constrained"
+            quality="90"
+          />
+        </Styled.ImageWrapper>
+        <div>
+          <Styled.Title>My Dictionary</Styled.Title>
+          <Styled.Dek>
+            web application to search English words and their meanings.
+          </Styled.Dek>
+          <Pill>React</Pill>
+          <Pill>Tailwind CSS</Pill>
+        </div>
+      </Styled.StyledLink>
+      <Styled.Masonry>
+        <Styled.MasonryColumn>
+          <Styled.StyledLink
+            to="https://github.com/nisalrenuja"
+            className="cover"
+          >
+            {/* <Styled.ImageWrapper>
                             <StaticImage
                                 src="../../images/work_list/cover.png"
                                 alt="Cover"
@@ -42,19 +41,19 @@ const WorkList = () => {
                                 quality="90"
                             />
                         </Styled.ImageWrapper> */}
-                        <div>
-                            <Styled.Title>Cover</Styled.Title>
-                            <Styled.Dek>Split the check in seconds.</Styled.Dek>
-                            <Pill>2015</Pill>
-                        </div>
-                    </Styled.StyledLink>
-                </Styled.MasonryColumn>
-                <Styled.MasonryColumn>
-                    <Styled.StyledLink
-                        to="/work/karma"
-                        className="karma"
-                    >
-                        {/* <Styled.ImageWrapper>
+            <div>
+              <Styled.Title>Cover</Styled.Title>
+              <Styled.Dek>Split the check in seconds.</Styled.Dek>
+              <Pill>2015</Pill>
+            </div>
+          </Styled.StyledLink>
+        </Styled.MasonryColumn>
+        <Styled.MasonryColumn>
+          <Styled.StyledLink
+            to="https://github.com/nisalrenuja"
+            className="karma"
+          >
+            {/* <Styled.ImageWrapper>
                             <StaticImage
                                 src="../../images/work_list/karma.png"
                                 alt="Karma Go"
@@ -63,23 +62,25 @@ const WorkList = () => {
                                 quality="90"
                             />
                         </Styled.ImageWrapper> */}
-                        <div>
-                            <Styled.Title>Karma</Styled.Title>
-                            <Styled.Dek>Simpler, friendlier Internet for everyone.</Styled.Dek>
-                            <Pill>2016</Pill>
-                        </div>
-                    </Styled.StyledLink>
-                    <Styled.StyledLink to="/work/etsy">
-                        <div>
-                            <Styled.Title>Etsy</Styled.Title>
-                            <Styled.Dek>People Power.</Styled.Dek>
-                            <Pill>2013–2015</Pill>
-                        </div>
-                    </Styled.StyledLink>
-                </Styled.MasonryColumn>
-            </Styled.Masonry>
-        </>
-    )
-}
+            <div>
+              <Styled.Title>Karma</Styled.Title>
+              <Styled.Dek>
+                Simpler, friendlier Internet for everyone.
+              </Styled.Dek>
+              <Pill>2016</Pill>
+            </div>
+          </Styled.StyledLink>
+          <Styled.StyledLink to="https://github.com/nisalrenuja">
+            <div>
+              <Styled.Title>Etsy</Styled.Title>
+              <Styled.Dek>People Power.</Styled.Dek>
+              <Pill>2013–2015</Pill>
+            </div>
+          </Styled.StyledLink>
+        </Styled.MasonryColumn>
+      </Styled.Masonry>
+    </>
+  );
+};
 
-export default WorkList
+export default WorkList;
